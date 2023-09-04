11079fdad712a6b1d31c62b4c938464ffc5bf702
Christopher Makarem <23037854+x24git@users.noreply.github.com>
2023-09-03 17:28:48 -0700

    Add Dark Mode

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ require("particles.js")
 import("jquery.easing")
 import * as bootstrap from 'bootstrap'
 
-particlesJS.load('particles-js', 'particles/particles.json', function() {});
+particlesJS.load('particles-js', 'particles/particles.json', function () {
+});
 
 
 //Enable Tooltips
@@ -13,21 +14,24 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 //Auto-collapse navbar on click
 const scrollTriggerList = document.querySelectorAll('.js-scroll-trigger')
 const navbar = document.querySelector('#navbarSupportedContent')
-const navbar_bs =  bootstrap.Collapse.getOrCreateInstance(navbar,{toggle: false})
+const navbar_bs = bootstrap.Collapse.getOrCreateInstance(navbar, {toggle: false})
 scrollTriggerList.forEach(function (scrollTriggerEl) {
     scrollTriggerEl.addEventListener('click', function () {
         //check if element is currently shown
         if (isNavbarShown) {
             navbar_bs.hide();
-        }})})
+        }
+    })
+})
 
 function resizeEvent() {
     for (let i = 0; i < 1000; i++) {
-        setTimeout(function() {
+        setTimeout(function () {
             window.dispatchEvent(new Event('resize'));
-        },  i)
+        }, i)
     }
 }
+
 const navbarToggler = document.querySelector('.navbar-toggler')
 navbarToggler.addEventListener('click', function () {
     navbar_bs.toggle();
@@ -47,10 +51,97 @@ navbar.addEventListener('show.bs.collapse', function () {
 const profilePic = document.querySelector('.img-profile')
 if (process.env.PROFILE !== undefined && process.env.PROFILE !== null) {
     profilePic.src = process.env.PROFILE
-}else {
+} else {
     profilePic.style.display = 'none';
 }
 
 //initialize navbar as hidden
 let isNavbarShown = false;
+
+
+const getStoredTheme = () => localStorage.getItem('theme')
+const setStoredTheme = theme => localStorage.setItem('theme', theme)
+
+const getPreferredTheme = () => {
+    const storedTheme = getStoredTheme()
+    if (storedTheme) {
+        return storedTheme
+    }
+
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+}
+const darkModeSwitch = document.getElementById('darkMode')
+const darkModeButton = document.getElementById('dark-button')
+const lightModeButton = document.getElementById('light-button')
+
+const setTheme = theme => {
+    const sunIcon = document.getElementById('sunIcon')
+    const moonIcon = document.getElementById('moonIcon')
+
+    if (theme === 'dark') {
+        console.log('dark mode enabled')
+        document.documentElement.setAttribute('data-bs-theme', 'dark')
+        darkModeSwitch.checked = true
+        sunIcon.classList.remove('active')
+        moonIcon.classList.add('active')
+        darkModeButton.style.display = 'block'
+        lightModeButton.style.display = 'none'
+
+    } else {
+        console.log('light mode enabled')
+        document.documentElement.setAttribute('data-bs-theme', theme)
+        darkModeSwitch.checked = false
+        sunIcon.classList.add('active')
+        moonIcon.classList.remove('active')
+        darkModeButton.style.display = 'none'
+        lightModeButton.style.display = 'block'
+    }
+}
+
+setTheme(getPreferredTheme())
+
+const changeTheme = (theme) => {
+    if (typeof theme !== 'string') {
+        theme = darkModeSwitch.checked ? 'dark' : 'light'
+    }
+    console.log('theme changed to ' + theme)
+    setTheme(theme)
+    // setStoredTheme(theme)
+}
+
+darkModeSwitch.addEventListener('change', changeTheme)
+
+darkModeButton.addEventListener('click', function () {
+    changeTheme('light')
+})
+
+lightModeButton.addEventListener('click', function () {
+    changeTheme('dark')
+})
+
+const topButton = document.getElementById("top-button");
+window.onscroll = function () {
+    scrollFunction();
+};
+const scrollFunction = () => {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+}
+
+const backToTop = () => {
+    window.scrollTo(0, 0)
+}
+
+// When the user clicks on the button, scroll to the top of the document
+topButton.addEventListener("click", backToTop);
+
+
+
+
 
--- src/js/countdown.js
+++ src/js/countdown.js
@@ -1,60 +1,59 @@
-(function($) {
-  
-  "use strict";  
+(function ($) {
 
-  $(window).on('load', function() {
+    "use strict";
 
-     /* Page Loader active
-    ========================================================*/
-    $('#preloader').fadeOut();
+    $(window).on('load', function () {
 
-  // Sticky Nav
-    $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 200) {
-            $('.scrolling-navbar').addClass('top-nav-collapse');
-        } else {
-            $('.scrolling-navbar').removeClass('top-nav-collapse');
-        }
-    });
+        /* Page Loader active
+       ========================================================*/
+        $('#preloader').fadeOut();
 
-
-
-    /* ==========================================================================
-    countdown timer
-    ========================================================================== */
-     jQuery('#clock').countdown('2020/05/15',function(event){
-      var $this=jQuery(this).html(event.strftime(''
-      +'<div class="time-entry days"><span>%-D</span> Days</div> '
-      +'<div class="time-entry hours"><span>%H</span> Hours</div> '
-      +'<div class="time-entry minutes"><span>%M</span> Minutes</div> '
-      +'<div class="time-entry seconds"><span>%S</span> Seconds</div> '));
-    });
+        // Sticky Nav
+        $(window).on('scroll', function () {
+            if ($(window).scrollTop() > 200) {
+                $('.scrolling-navbar').addClass('top-nav-collapse');
+            } else {
+                $('.scrolling-navbar').removeClass('top-nav-collapse');
+            }
+        });
 
 
-    // one page navigation 
-    $('.onepage-nev').onePageNav({
+        /* ==========================================================================
+        countdown timer
+        ========================================================================== */
+        jQuery('#clock').countdown('2020/05/15', function (event) {
+            var $this = jQuery(this).html(event.strftime(''
+                + '<div class="time-entry days"><span>%-D</span> Days</div> '
+                + '<div class="time-entry hours"><span>%H</span> Hours</div> '
+                + '<div class="time-entry minutes"><span>%M</span> Minutes</div> '
+                + '<div class="time-entry seconds"><span>%S</span> Seconds</div> '));
+        });
+
+
+        // one page navigation
+        $('.onepage-nev').onePageNav({
             currentClass: 'active'
-    }); 
+        });
 
-    /* Back Top Link active
-    ========================================================*/
-      var offset = 200;
-      var duration = 500;
-      $(window).scroll(function() {
-        if ($(this).scrollTop() > offset) {
-          $('.back-to-top').fadeIn(400);
-        } else {
-          $('.back-to-top').fadeOut(400);
-        }
-      });
+        /* Back Top Link active
+        ========================================================*/
+        var offset = 200;
+        var duration = 500;
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > offset) {
+                $('.back-to-top').fadeIn(400);
+            } else {
+                $('.back-to-top').fadeOut(400);
+            }
+        });
 
-      $('.back-to-top').on('click',function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: 0
-        }, 600);
-        return false;
-      });
+        $('.back-to-top').on('click', function (event) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: 0
+            }, 600);
+            return false;
+        });
 
-  });
+    });
 }(jQuery));
